d7e4efe695bd5b2ac8e726b71d9d5c32d4cc7d5b
Ngọc Phạm Xuân <ngoc.phamxuan2001@gmail.com>
2023-01-31 19:20:37 +0700

    add role server

--- src/constant/ServerPolicy.js
+++ src/constant/ServerPolicy.js
@@ -3,6 +3,10 @@ const serverPolicy = {
   OWNER: 0,
   // can modify description, kick user, change typeofserver, change name of server
   MANAGE_SERVER: 1,
+  // allow default members see channel(not private channel)
+  READ_CHANNEL: 5,
+  // allow members CRUD channel
+  MANAGE_CHANNEL: 6,
   // create role group, add policy
   MANAGE_ROLE: 2,
   MANAGE_INVITE: 3,
