d6b8cd06312901dc443d737a26150c2135dac49f
Ngọc Phạm Xuân <ngoc.phamxuan2001@gmail.com>
2023-01-31 22:49:12 +0700

    fix message

--- src/routes/message.router.js
+++ src/routes/message.router.js
@@ -5,7 +5,8 @@ const ChannelRoleController = require('../controllers/channelRole.controller');
 const MessageController = require('../controllers/message.controller');
 const Authen = require('../middleware/authen');
 
-router.post('/:channelId', Authen.verifyToken, Authen.verifyPermission(channelPolicy.CREATE_MESSAGE), MessageController.sendMessage)
+router.post('/:channelId', Authen.verifyToken, MessageController.sendMessage)
+// Authen.verifyPermission(channelPolicy.CREATE_MESSAGE)
 router.get('/:channelId', Authen.verifyToken, MessageController.getAllMessages)
 // Authen.verifyPermission(channelPolicy.READ)
 module.exports = router;
